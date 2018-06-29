eabf10bfb64d328567042c2f1839f855fa8b6ca7
wuhuo123 <2155901540@qq.com>
2018-06-29 10:52:08 +0800

    添加mapper的扫描路径

--- src/main/java/com/cloud/store/StoreApplication.java
+++ src/main/java/com/cloud/store/StoreApplication.java
@@ -1,9 +1,11 @@
 package com.cloud.store;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cloud.store.mapper*")
 public class StoreApplication {
 
 	public static void main(String[] args) {
